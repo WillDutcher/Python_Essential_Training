a0c66defd2f800e04a7fa7c8f41232a83a79c50d
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-22 10:40:31 -0400

    Completed Chap02: Lesson 5 - 'Using print()'

--- Chap02/hello.py
+++ Chap02/hello.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+x = ", because I can."
 print('Hello, World.')
 print('something entirely different')
+print('I am going to add my own variable{}'.format(x))
+print('I am adding a separate way, a Python 2 way%s' % x)
+print(f'Lastly, here\'s a final way{x}')
