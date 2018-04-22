41507ad2c229b5fa0bc51c32ff45415ef55fdd9c
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-22 11:28:34 -0400

    Completed Chap02: Lesson 9 - 'Functions'

--- Chap02/function.py
+++ Chap02/function.py
@@ -3,5 +3,14 @@
 
 def function(n):
     print(n)
+    return n * 2
 
 function(47)
+
+def newFunc(*args): # Pass a non-static number of variables
+    print(*args)
+
+newFunc(1, 2, 3, 'potato')
+
+x = function(42)
+print(x)
--- Chap02/primes.py
+++ Chap02/primes.py
@@ -10,9 +10,30 @@ def isprime(n):
     else:
         return True
 
+def list_primes():
+    for y in range(101):
+        if isprime(y):
+            print(y, end = ' ', flush = True)
+
+def isprime2(x):
+    if x <= 1:
+        return False
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+    else:
+        return True
+
 n = 5
+x = int(input("Give me a number: "))
 if isprime(n):
     print(f'{n} is prime')
 else:
     print(f'{n} not prime')
 
+if isprime2(x):
+    print(f'{x} is prime')
+else:
+    print(f'{x} not prime')
+
+list_primes()
