b526fe8a73f7a8f31f60038b1bc001b219f376df
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-22 10:55:33 -0400

    Completed Chap02: Lesson 7 - 'Conditionals'

--- Chap02/conditionals.py
+++ Chap02/conditionals.py
@@ -7,3 +7,18 @@ y = 42
 if x < y:
     print('x < y: x is {} and y is {}'.format(x, y))
 
+# Above is same as this:
+print("Another way: On one line\n")
+print('if x < y: print(\'x < y: x is {} and y is {}\'.format.(x, y))')
+if x < y: print('x < y: x is {} and y is {}'.format(x, y)) # Frowned upon to do it this way
+
+print('\n' + '*'*50 + '\n')
+
+if x > y:
+    print('x > y: x is {} and y is {}'.format(x, y))
+elif x < y:
+    print('x < y: x is {} and y is {}'.format(x, y))
+elif x == y:
+    print('x == y: x is {} and y is {}'.format(x, y))
+else:
+    print('Do something else.')
