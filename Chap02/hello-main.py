4038a5b2297ad110e09ffa5d3d3ecd45a9cf5c67
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-22 10:35:02 -0400

    Completed Chap02: Lesson 4 - 'Whitespace and comments'

--- Chap02/hello-main.py
+++ Chap02/hello-main.py
@@ -8,6 +8,17 @@ def main():
 
 def message():
     print('This is python version {}'.format(platform.python_version()))
+    new()
+
+def new():
+    print('\nI just wanted to have something to add. So here it is.')
+    last()
+
+def last():
+    if True:
+        print("This is a true statement.")
+    else:
+        print("This statement is a false one.")
 
 # By having this conditional statement, below, that calls main
 # it actually forces the interpreter to read the entire script
