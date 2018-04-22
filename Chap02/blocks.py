cacfe3eb712e76052ac97cb0e8e9f58dcc690d59
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-22 10:45:49 -0400

    Completed Chap02: Lesson 6 - 'Blocks and scope'

--- Chap02/blocks.py
+++ Chap02/blocks.py
@@ -5,4 +5,12 @@ x = 42
 y = 73
 
 if x < y:
+    z = 112
     print('x < y: x is {} and y is {}'.format(x, y))
+    print('line 2') # Only prints if 'if' statement is true
+    print('line 3') # Only prints if 'if' statement is true
+    print('line 4') # Only prints if 'if' statement is true
+print('line 5') # Prints automatically because it's not part of block, above, now
+
+print('Something else.')
+print('z is equal to: {}'.format(z))
