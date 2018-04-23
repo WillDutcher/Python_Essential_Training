524d1cb543a9c8ead2b41e3b4d05e7a4686f0caf
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-23 19:54:33 -0400

    Completed Chap05: Lesson 20 - Arithmetic operators'

--- Chap05/arithmetic.py
+++ Chap05/arithmetic.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+# Completed Chap05: Lesson 20 - Arithmetic operators
+
 x = 5
 y = 3
-z = x + y
+plus = x + y
+minus = x - y
+times = x * y
+divide = x / y
+modulo = x // y
+power = x ** y
 
-print(f'result is {z}')
+print("x = {} and y = {}".format(x, y))
+print(f'plus result is {plus}')
+print(f'minus result is {minus}')
+print(f'times result is {times}')
+print(f'divide result is {divide}')
+print(f'modulo result is {modulo}')
+print(f'power result is {power}')
