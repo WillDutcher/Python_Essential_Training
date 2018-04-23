6f1e38f3ae238167778930258bdde258083c8022
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-23 19:30:59 -0400

    Completed Chap04: Lesson 17 - 'Conditional Syntax'

--- Chap04/conditional.py
+++ Chap04/conditional.py
@@ -7,3 +7,20 @@ elif False:
     print('elif true')
 else:
     print('neither true')
+
+print("*"*50)
+
+x = 3
+
+if x == 0:
+    print("zero is true")
+elif x == 1:
+    print("one is true")
+elif x == 2:
+    print("two is true")
+elif x == 3:
+    print("three is true")
+elif x == 4:
+    print("four is true")
+else:
+    print("none is true")
