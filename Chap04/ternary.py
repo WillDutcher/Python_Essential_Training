f22b30a625cda5aeb3e11e99e740f45a6d4ec742
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-23 19:43:48 -0400

    Completed Chap04: Lesson 19 - 'Conditional assignment'

--- Chap04/ternary.py
+++ Chap04/ternary.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+count = 0
 hungry = True
 x = 'Feed the bear now!' if hungry else 'Do not feed the bear.'
 print(x)
+
+while count < 5:
+    hungry = True
+    if hungry == False:
+        print("You've fed the bear enough. It is full.")
+    elif count == 4:
+        print("You've fed the bear enough. It is full.")
+    else:
+        print("You're feeding the bear.")
+    count += 1
