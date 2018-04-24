38e1187d0a935c5d1df28ff5b8bbbaec896b8423
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-23 20:08:16 -0400

    Completed Chap05: Lesson 22 - Comparison operators

--- Chap05/comparison.py
+++ Chap05/comparison.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+# Completed Chap05: Lesson 22 - Comparison operators
+
+print("<\tLess than")
+print(">\tGreater than")
+print("<=\tLess than or equal to")
+print(">=\tGreater than or equal to")
+print("==\tEqual")
+print("!=\tNot equal\n")
+print("*"*50,"\n")
+
 x = 42
 y = 73
 
@@ -8,4 +18,3 @@ if x < y:
     print('comparison is true')
 else:
     print('comparison is false')
-
