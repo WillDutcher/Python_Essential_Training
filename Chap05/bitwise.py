a97fe5e9d01e2c42701396e9fe237e5ee0f1032b
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-23 20:04:23 -0400

    Completed Chap05: Lesson 21 - Bitwise operators

--- Chap05/bitwise.py
+++ Chap05/bitwise.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+# Completed Chap05: Lesson 21 - Bitwise operators
+
 x = 0x0a
 y = 0x02
 z = x & y
 
+# 02x gives two-character string, hexadecimal, with leading zero
+# 0 = leading zero
+# 2 = two characters wide
+# x = hexadecimal display of integer value
+# 08b gives eight-character string, binary, with leading zeroes
+# 0 = leading zeroes, where applicable
+# 8 = 8 characters wide
+# b = binary display of integer value
 print(f'(hex) x is {x:02x}, y is {y:02x}, z is {z:02x}')
 print(f'(bin) x is {x:08b}, y is {y:08b}, z is {z:08b}')
+
+print("&\tAnd")
+print("|\tOr")
+print("^\tXor")
+print("<<\tShift left")
+print(">>\tShift right")
