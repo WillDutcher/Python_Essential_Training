e3a985cc6739e5250e8cbc886aa2f86d68de0927
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-28 09:40:39 -0400

    Completed Chap07: Lesson 31 - 'Keyword arguments'

--- Chap07/kwargs.py
+++ Chap07/kwargs.py
@@ -3,11 +3,28 @@
 
 def main():
     kitten(Buffy = 'meow', Zilla = 'grr', Angel = 'rawr')
+    dog(Rex = "woof", Spot = "ruff", Daisy = "arf")
+    x = dict(Birdie = "tweet", Tweety = "tweep", Canary = "gleep")
+    bird(**x)
 
 def kitten(**kwargs):
     if len(kwargs):
         for k in kwargs:
             print('Kitten {} says {}'.format(k, kwargs[k]))
     else: print('Meow.')
+
+def dog(**kwargs):
+    print('\n')
+    if len(kwargs):
+        for k in kwargs:
+            print("The dog named {} says {}!".format(k, kwargs[k]))
+    else: print("Yip!")
+
+def bird(**kwargs):
+    print('\n')
+    if len(kwargs):
+        for k in kwargs:
+            print("The bird named {} says {}!".format(k, kwargs[k]))
+    else: print('Caw!')
 
 if __name__ == '__main__': main()
