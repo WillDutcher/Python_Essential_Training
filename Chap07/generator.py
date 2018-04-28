12e40cd855c965bd6276bd24abb4b9a6aeaa7a7a
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-28 10:03:46 -0400

    Completed Chap07: Lesson 32 - 'Generators'

--- Chap07/generator.py
+++ Chap07/generator.py
@@ -10,7 +10,7 @@ def inclusive_range(*args):
     numargs = len(args)
     start = 0
     step = 1
-    
+
     # initialize parameters
     if numargs < 1:
         raise TypeError(f'expected at least 1 argument, got {numargs}')
@@ -25,7 +25,36 @@ def inclusive_range(*args):
     # generator
     i = start
     while i <= stop:
-        yield i
+        yield i # Works like a return, but for a generator
         i += step
+
+if __name__ == '__main__': main()
+
+def main():
+    for x in my_range(10,100,10):
+        print(x, end = ' ')
+    print()
+
+def my_range(*args):
+    totalArgs = len(args)
+    myStart = 0
+    mySkip = 1
+
+    # Initialize all parameters
+    if totalArgs < 1:
+        raise TypeError(f'\nI expected you to give me at least one argument. You gave me {totalArgs}')
+    elif totalArgs == 1:
+        myStop = args[0]
+    elif totalArgs == 2:
+        (myStart, myStop) = args
+    elif totalArgs == 3:
+        (myStart, myStop, mySkip) = args
+    else: raise TypeError(f'\nI expected you to give me at most three arguments. You gave me {totalArgs}')
+
+    # generator
+    i = myStart
+    while i <= myStop:
+        yield i
+        i += mySkip
 
 if __name__ == '__main__': main()
