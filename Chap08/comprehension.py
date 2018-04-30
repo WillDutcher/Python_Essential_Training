7aa4280291fb34405de5931216298114cd955d8d
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-29 21:18:35 -0400

    Completed Chap08: Lesson 37 - 'List comprehension'

--- Chap08/comprehension.py
+++ Chap08/comprehension.py
@@ -2,8 +2,22 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 def main():
+    from math import pi
     seq = range(11)
+    print(seq)
+    seq2 = [x * 2 for x in seq]
+    seq3 = [x for x in seq if x % 3 != 0]
+    seq4 = [(x, x**2) for x in seq]
+    seq5 = [round(pi, i) for i in seq]
+    seq6 = {x:x**2 for x in seq}
+    seq7 = {x for x in 'superduper' if x not in 'pd'}
     print_list(seq)
+    print_list(seq2)
+    print_list(seq3)
+    print_list(seq4)
+    print_list(seq5)
+    print(seq6)
+    print_list(seq7)
 
 def print_list(o):
     for x in o: print(x, end = ' ')
