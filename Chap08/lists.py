cbb64a8d415c57d2a1fc49bc862a25827d5371b7
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-29 20:24:31 -0400

    Completed Chap08: Lesson 34 - 'Lists and tuples'

--- Chap08/lists.py
+++ Chap08/lists.py
@@ -3,7 +3,27 @@
 
 def main():
     game = [ 'Rock', 'Paper', 'Scissors', 'Lizard', 'Spock' ]
+    print(game[2:4]) # Prints items 3 and 4 in the list, above
+    print(game[0:5:2])
+    i = game.index('Paper')
+    print(game[i])
+    game.append('Computer')
+    game.insert(0,'Corn')
     print_list(game)
+    game.remove('Corn')
+    x = game.pop() # Remove end item
+    print(x)
+    y = game.pop(0)
+    print(y)
+    del game[0:2]
+    print(game)
+    print(', '.join(game))
+    game.append('New One A')
+    game.append('New One B')
+    game.append('New One C')
+    print(game)
+    print(len(game))
+
 
 def print_list(o):
     for i in o: print(i, end=' ', flush=True)
