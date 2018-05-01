aa5c1bdc4974d2a6606faf1e8517b8bdef92c854
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-30 22:04:05 -0400

    Completed Chap10: Lesson 44 - 'Handling exceptions'

--- Chap10/hello.py
+++ Chap10/hello.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+import sys
+
 def main():
-    print('Hello, World.')
+    try:
+        x = a5/2
+    except ValueError:
+        print("I caught a ValueError.")
+    except ZeroDivisionError:
+        print('Don\'t divide by zero!')
+    except:
+        print(f'Unknown error: {sys.exc_info()[1]}')
+    else:
+        print("No errors found.")
+        print(x)
 
 if __name__ == '__main__': main()
