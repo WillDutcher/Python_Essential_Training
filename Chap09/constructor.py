a7b7f99b905c4cc55c6148e090267d005087c52f
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-30 21:13:49 -0400

    Completed Chap09: Lesson 39 - 'Constructing an object'

--- Chap09/constructor.py
+++ Chap09/constructor.py
@@ -6,6 +6,11 @@ class Animal:
         self._type = type
         self._name = name
         self._sound = sound
+        # Another way to do this:
+    # def __init__(self, **kwargs):
+        # self._type = kwargs['type'] if 'type' in kwargs else 'kitten' # with a default value of 'kitten'
+        # self._name = kwargs['name'] if 'name' in kwargs else 'fluffy' # with a default value of 'fluffy'
+        # self._sound = kwargs['sound'] if 'sound' in kwargs else 'rawr' # with a default value of 'rawr'
 
     def type(self):
         return self._type
@@ -23,10 +28,13 @@ def print_animal(o):
 
 
 def main():
-    a0 = Animal('kitten', 'fluffy', 'rwar')
+    a0 = Animal('kitten', 'fluffy', 'rawr')
+    # Alternate way, re: above
+    # a0 = Animal(type="kitten", name="fluffy", sound="rawr")
     a1 = Animal('duck', 'donald', 'quack')
     print_animal(a0)
     print_animal(a1)
     print_animal(Animal('velociraptor', 'veronica', 'hello'))
+    # print_animal(Animal()) # prints default values
 
 if __name__ == '__main__': main()
