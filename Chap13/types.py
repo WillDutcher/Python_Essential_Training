bc7949d255df10cf8d54cba0e1e70dd25639881c
Will Dutcher <william.c.dutcher@gmail.com>
2018-05-11 19:36:03 -0400

    Completed Chap13: Lesson 56 - Object and class functions

--- Chap13/types.py
+++ Chap13/types.py
@@ -82,3 +82,15 @@ for i, v in enumerate(f): print(f'{i}: {v}')
 
 # Object and Class Functions
 print('\n')
+x = 42
+xx = 42.0
+y = type(x)
+z = isinstance(x, int)
+zz = isinstance(xx, int)
+a = id(x)
+print(x)
+print(y)
+print(z)
+print(zz)
+print(a)
+print(id(xx))
