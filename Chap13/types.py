b2b6365598d8e207729267c0f378e852b962d737
Will Dutcher <william.c.dutcher@gmail.com>
2018-05-11 18:58:28 -0400

    Completed Chap13: Lesson 53 - Numeric functions

--- Chap13/types.py
+++ Chap13/types.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+# Numeric Functions
 x = '47'
 y = int(x)
+z = float(x)
+zz = abs(y) # Needs to be y, lest I change to 'abs(int(x))'
+zzz = divmod(y, 3) # Same as above, re: y vice x
+zzzz = complex(y, 72)
 
 print(f'x is {type(x)}')
 print(f'x is {x}')
 print(f'y is {type(y)}')
 print(f'y is {y}')
+print(f'z is {type(z)}')
+print(f'z is {z}')
+print(f'zz is {type(zz)}')
+print(f'zz is {zz}')
+print(f'zzz is {type(zzz)}')
+print(f'zzz is {zzz}')
+print(f'zzzz is {type(zzzz)}')
+print(f'zzzz is {zzzz}')
+
+
+# String Functions
+
+
+# Container Functions
+
+
+# Object and Class Functions
