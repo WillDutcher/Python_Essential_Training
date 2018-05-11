606988ec13f67509eb7f55ce5e227134a6d40e22
Will Dutcher <william.c.dutcher@gmail.com>
2018-05-11 17:56:51 -0400

    Completed Chap12: Lesson 50 - Opening files

--- Chap12/files.py
+++ Chap12/files.py
@@ -2,8 +2,12 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 def main():
-    f = open('lines.txt')
+    f = open('lines.txt') # Default to open in read-only mode ('r')
+    # w: If you open in write mode, it will empty/clear the file and start anew
+    # a: If you open in append mode, it will begin writing at the end of last line
+    # +: Read and write mode (ex: r+)
+    # b or t: Binary or text mode (ex: r+b or r+t)
     for line in f:
-        print(line.rstrip())
+        print(line.rstrip()) # This strips any whitespace and newlines for each line
 
 if __name__ == '__main__': main()
