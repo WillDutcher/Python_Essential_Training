86829bf6e5260914162705f6304c3719d2086bb0
Will Dutcher <william.c.dutcher@gmail.com>
2018-05-11 19:16:17 -0400

    Completed Chap13: Lesson 54 - String functions

--- Chap13/types.py
+++ Chap13/types.py
@@ -22,11 +22,33 @@ print(f'zzz is {zzz}')
 print(f'zzzz is {type(zzzz)}')
 print(f'zzzz is {zzzz}')
 
-
 # String Functions
+print('\n')
+s = 'Hello, World.'
+print(repr(s)) # repr = representation; raw string, itself
 
+class bunny:
+    # The way this works:
+        # Prints the __str__ version if you do print(s), otherwise
+        # Will print __repr__ version, if print(repr(s)), otherwise
+        # Will print memory address of the object
+    def __init__(self, n):
+        self._n = n
+    def __repr__(self):
+        return f'repr: The number of bunnies is {self._n}'
+    def __str__(self):
+        return f'str: The number of bunnies is {self._n}'
+
+s = bunny(47)
+print(repr(s)) # Prints the bunny object memory address, *UNLESS* repr method defined (__repr__)
+print(s) # Same as if I'd put print(str(s))
+print(ascii(s)) # Works just like repr, but will escape special characters for unicode emojis (as example)
+# print(chr(128406)) # Live long and prosper emoji hand; won't display in cygwin
+# ord = number of a character (emoji) whereas chr gives you character (emoji) of a number
 
 # Container Functions
+print('\n')
 
 
 # Object and Class Functions
+print('\n')
