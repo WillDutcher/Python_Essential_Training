71fcd45b03d6f2b57f929a11130407f26e712c61
Will Dutcher <william.c.dutcher@gmail.com>
2018-05-11 12:57:45 -0400

    Completed Chap11: Lesson 46 - Overview of string objects

--- Chap11/hello.py
+++ Chap11/hello.py
@@ -2,3 +2,18 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 print('Hello, World.')
+print('Hello, World.'.upper())
+print('Hello, World.'.swapcase())
+print('Hello, World.{}'.format(42 * 7))
+print("""
+    Hello,
+    World.
+
+    {}""".format(1+2))
+
+class MyString(str):
+    def __str__(self):
+        return self[::-1]
+
+s = MyString('Hello, World.')
+print(s)
