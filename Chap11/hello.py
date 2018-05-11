f2f57d98a620c44036354cc229facd03c392917a
Will Dutcher <william.c.dutcher@gmail.com>
2018-05-11 13:13:39 -0400

    Completed Chap11: Lesson 48 - Formatting strings

--- Chap11/hello.py
+++ Chap11/hello.py
@@ -38,3 +38,24 @@ t2 = ', how are ya?'
 t3 = 'this string' ' that string'
 print(t1 + t2)
 print(t3)
+
+print('\n')
+################################################
+print('\n')
+
+x = 42
+y = 73
+print("the number is: {} {}".format(x, y))
+print("the number is: {abc} {defg}".format(abc = x, defg = y))
+print("the number is: {1} {0}".format(x, y))
+print("the number is: {0:<5} {1:>5}".format(x, y))
+z = 43 * 4234
+print("The number is {:,}".format(z))
+print("The number is {:,}".format(z).replace(',','.'))
+print("The number is {:.3f}".format(z))
+a = 42
+print("the number is: {:x}".format(x)) # HEXADECIMAL
+print("the number is: {:o}".format(x)) # OCTAL
+print("the number is: {:b}".format(x)) # BINARY
+print(f'The number is: {x}')
+print(f'The number is: {x:.3f}')
