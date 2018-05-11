340a954ac1007d1e56fd700da2cef9124a3fba15
Will Dutcher <william.c.dutcher@gmail.com>
2018-05-11 18:16:25 -0400

    Completed Chap12: Lesson 51 - Text files

--- Chap12/copy-text.py
+++ Chap12/copy-text.py
@@ -6,8 +6,10 @@ def main():
     outfile = open('lines-copy.txt', 'wt')
     for line in infile:
         print(line.rstrip(), file=outfile)
+        # outfile.writelines(line) # This works same as above line, just doesn't print
         print('.', end='', flush=True)
     outfile.close()
+    infile.close()
     print('\ndone.')
 
 if __name__ == '__main__': main()
