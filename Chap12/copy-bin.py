5e7ce7a7cbe68444a73cba3d1f028e1ffdf0ab51
Will Dutcher <william.c.dutcher@gmail.com>
2018-05-11 18:20:39 -0400

    Completed Chap12: Lesson 52 - Binary files

--- Chap12/copy-bin.py
+++ Chap12/copy-bin.py
@@ -2,10 +2,10 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 def main():
-    infile = open('berlin.jpg', 'rb')
+    infile = open('berlin.jpg', 'rb') # Would get error if try 'rt' as this is binary and not text file
     outfile = open('berlin-copy.jpg', 'wb')
     while True:
-        buf = infile.read(10240)
+        buf = infile.read(10240) # reading in 10k byte chunks; not wise to read entire file at once
         if buf:
             outfile.write(buf)
             print('.', end='', flush=True)
