e6807fb8757cd09984757a932b1a55a09f3a7f52
Will Dutcher <william.c.dutcher@gmail.com>
2018-05-11 13:02:41 -0400

    Completed Chap11: Lesson 47 - Common string methods

--- Chap11/hello.py
+++ Chap11/hello.py
@@ -17,3 +17,24 @@ class MyString(str):
 
 s = MyString('Hello, World.')
 print(s)
+
+print('\n')
+################################################
+print('\n')
+
+print('Hello, World.'.upper())
+print('Hello, World.'.lower())
+print('Hello, World.'.capitalize())
+print('Hello, World.'.swapcase())
+print('Hello, World.'.casefold())
+
+s1 = "Hello, World."
+s2 = s1.upper()
+print(id(s1))
+print(id(s2))
+
+t1 = 'Hi'
+t2 = ', how are ya?'
+t3 = 'this string' ' that string'
+print(t1 + t2)
+print(t3)
