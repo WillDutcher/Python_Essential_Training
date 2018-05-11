f3411ce8a237640a96f857d2f4a687bab3f786bf
Will Dutcher <william.c.dutcher@gmail.com>
2018-05-11 13:18:14 -0400

    Completed Chap11: Lesson 49 - Splitting and joining

--- Chap11/split-join.py
+++ Chap11/split-join.py
@@ -3,3 +3,10 @@
 
 s = 'This is a long string with a bunch of words in it.'
 print(s)
+print(s.split()) # Split; each element in string is now a word
+print(s.split('i')) # Split on letter 'i'
+l = s.split()
+s2 = ':'.join(l)
+print(s2)
+s3 = ''.join(l)
+print(s3)
