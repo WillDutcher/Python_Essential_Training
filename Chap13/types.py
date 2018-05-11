d67326fb34bf757981d34fe754dda64012d275d5
Will Dutcher <william.c.dutcher@gmail.com>
2018-05-11 19:32:55 -0400

    Completed Chap13: Lesson 55 - Container functions

--- Chap13/types.py
+++ Chap13/types.py
@@ -48,7 +48,37 @@ print(ascii(s)) # Works just like repr, but will escape special characters for u
 
 # Container Functions
 print('\n')
-
+x = (1, 2, 3, 4, 5)
+y = x
+z = len(x)
+a = list(reversed(x)) # otherwise: a = reversed(x) will show memory location with print(a)
+b = reversed(x)
+c = sum(x)
+cc = sum(x, 10)
+cmax = max(x)
+cmin = min(x)
+d = any(x) # Returns TRUE if ANY value in string/tuple is true
+xx = (0, 0, 0, 0, 0)
+dd = any(xx)
+ddd = all(x) # Returns TRUE if ALL values in string/tuple is true
+dddd = all(xx)
+e = (6, 7, 8, 9, 10)
+eee = (15, 14, 13, 12, 11)
+ee = zip(x, e, eee)
+f = ('cat', 'dog', 'rabbit', 'velociraptor')
+print(x)
+print(y)
+print(z)
+print(a)
+for i in b: print(i)
+print(c)
+print(cc)
+print("cmax: {0}\tcmin: {1}".format(cmax, cmin))
+print("x: {}\txx: {}".format(x, xx))
+print("any for x: {0}\t\tany for xx: {1}".format(d, dd))
+print("all for x: {0}\t\tall for xx: {1}".format(ddd, dddd))
+for a, b, c in ee: print(f'{a} - {b} - {c}')
+for i, v in enumerate(f): print(f'{i}: {v}')
 
 # Object and Class Functions
 print('\n')
