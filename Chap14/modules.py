fc18c8ca3b143fef55bf9838a34affd3461b77a0
Will Dutcher <william.c.dutcher@gmail.com>
2018-05-11 22:17:18 -0400

    Completed Chap14: Lesson 57 - Using standard modules

--- Chap14/modules.py
+++ Chap14/modules.py
@@ -2,9 +2,34 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 import sys
+import os
+import random
+import datetime
 
 def main():
     v = sys.version_info
+    x = sys.platform
+    y = os.name
+    z = os.getenv('PATH')
+    a = os.getcwd()
+    b = os.urandom(25) # Random number 25 bytes long
+    bb = os.urandom(25).hex() # Random number 25 bytes long
+    c = random.randint(1, 1000)
+    d = list(range(25))
+    now = datetime.datetime.now()
     print('Python version {}.{}.{}'.format(*v))
+    print(x)
+    print(y)
+    print(z)
+    print('\n', a)
+    print(b)
+    print(bb)
+    print(c)
+    print('d: {}'.format(d))
+    random.shuffle(d)
+    print(d)
+    print(now)
+    print(now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond)
+
 
 if __name__ == '__main__': main()
